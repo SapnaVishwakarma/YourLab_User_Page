ac51f8847d543f2d9078ab55947710ea827eb953
Pavan <129081053+Pavanpraja@users.noreply.github.com>
2024-07-23 19:48:28 +0530

    updated something

--- fronted/src/pages/help.js
+++ fronted/src/pages/help.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Help = () => {
   return (
-    <div>
+    <div className='font-bold text-[2rem]'>
       Have you any need you can feel free to contact us...
     </div>
   )
